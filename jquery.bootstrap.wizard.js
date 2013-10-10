abf7e7d6efc568092c0a38ea7df022271263700a
Vincent Gabriel <vadimg88@gmail.com>
2013-10-09 17:17:58 -0700

    Update jquery.bootstrap.wizard.js

--- jquery.bootstrap.wizard.js
+++ jquery.bootstrap.wizard.js
@@ -3,6 +3,7 @@
  * Examples and documentation at: http://github.com/VinceG/twitter-bootstrap-wizard
  * version 1.0
  * Requires jQuery v1.3.2 or later
+ * Supports Bootstrap 2.2.x, 2.3.x, 3.0
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
