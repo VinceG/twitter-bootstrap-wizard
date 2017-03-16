d79e9a2e90c07e5360a980690b94144063367f17
Vincent Gabriel <vadimg88@gmail.com>
2017-03-16 08:33:36 -0700

    Updated version inside the js file

--- jquery.bootstrap.wizard.js
+++ jquery.bootstrap.wizard.js
@@ -1,7 +1,7 @@
 /*!
  * jQuery twitter bootstrap wizard plugin
  * Examples and documentation at: http://github.com/VinceG/twitter-bootstrap-wizard
- * version 1.3.1
+ * version 1.4.2
  * Requires jQuery v1.3.2 or later
  * Supports Bootstrap 2.2.x, 2.3.x, 3.0
  * Dual licensed under the MIT and GPL licenses:
@@ -329,7 +329,7 @@ $.fn.bootstrapWizard.defaults = {
 	previousSelector: '.wizard li.previous',
 	firstSelector:    '.wizard li.first',
 	lastSelector:     '.wizard li.last',
-  	finishSelector:   '.wizard li.finish',
+  finishSelector:   '.wizard li.finish',
 	backSelector:     '.wizard li.back',
 	onShow:           null,
 	onInit:           null,
@@ -337,8 +337,8 @@ $.fn.bootstrapWizard.defaults = {
 	onPrevious:       null,
 	onLast:           null,
 	onFirst:          null,
-  	onFinish:         null,
-  	onBack:           null,
+  onFinish:         null,
+  onBack:           null,
 	onTabChange:      null,
 	onTabClick:       null,
 	onTabShow:        null
